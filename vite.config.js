45ff86896d6b36506a07c1dc9dc9c245da7b4903
Mateus Developer <mateusm.developer@gmail.com>
2024-08-04 14:54:56 -0300

    Changing the 'vite.config.js' file, adding entries from '.html' files

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path';
 
 export default defineConfig({
   build: {
+    rollupOptions: {
+      input: {
+        index: resolve(__dirname, 'index.html'),
+        app: resolve(__dirname, 'app.html'),
+      }
+    },
     outdir: '../../dist'
-  },
-  
+  }
 })
